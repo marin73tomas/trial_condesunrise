241a1fd84fb80ab2a5a82588d9f15c123c34e7a8
= <=>
2021-07-10 17:42:34 -0400

    done banner desktop

--- wp-content/themes/astra-child/homepage.php
+++ wp-content/themes/astra-child/homepage.php
@@ -10,15 +10,13 @@
 get_header(); ?>
 
 <!-- Content -->
-<div class="container">
+
      <?php
 
 
      include_once('template_parts/home_carousel.php');
 
      ?>
-
-</div>
 
 
 
--- wp-content/themes/astra-child/template_parts/home_carousel.php
+++ wp-content/themes/astra-child/template_parts/home_carousel.php
@@ -1,37 +1,38 @@
-<div id="carouselHome" class="carousel slide" data-bs-ride="carousel">
+<div id="carouselHome" class="carousel slide  w-100" data-bs-ride="carousel">
      <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselHome" data-bs-slide-to="2" aria-label="Slide 3"></button>
      </div>
-     <div class="carousel-inner position-relative">
+     <div class="carousel-inner h-100">
 
           <?php for ($i = 0; $i < 3; $i++) { ?>
-               <div class="carousel-item <?php if ($i == 0) echo 'active'; ?>">
+               <div class="carousel-item h-100 <?php if ($i == 0) echo 'active'; ?>">
+                    <div class="container h-100 mx-auto">
+                         <div class="row h-100 justify-content-around">
+                              <div class="col-12 col-md-7">
+                                   <h2 class='main-gray-color fw-bold'>ENGAGED <span class='main-red-color'> EMPLOYEES. </span>
+                                        SEAMLESS <span class='main-red-color'> EXPERIENCE. </span>
+                                        SECURE <span class='main-red-color'> TECHNOLOGY. </span> </h2>
+                                   <h3 class='main-gray-color2 mt-3 f-semibold'>Trusted Managed IT Company</h3>
+                              </div>
+                              <div class="col-12 col-md-5 d-flex flex-column align-items-center mt-1">
+                                   <img class="d-block mx-auto" src="/wp-content/uploads/2021/07/Grupa-16@2x.png" alt="play-button">
+                                   <p id="play-video-txt" class="mt-3 text-center text-white fw-bold">PLAY THE VIDEO</p>
+                              </div>
+                         </div>
 
-                    <div class="row justify-content-around">
-                         <div class="col-6 col-sm-12">
-                              <h2>ENGAGED <span class='main-red-color'> EMPLOYEES. </span>
-                                   SEAMLESS <span class='main-red-color'> EXPERIENCE. </span>
-                                   SECURE <span class='main-red-color'> TECHNOLOGY. </span> </h2>
-                              <h3 class='main-gray-color2'>Trusted Managed IT Company</h3>
-                         </div>
-                         <div class="col-6 col-sm-12 d-flex flex-column align-items-center">
-                              <img src="/wp-content/uploads/2021/07/Grupa-16@2x.png" alt="play-button">
-                         </div>
                     </div>
-
                </div>
 
 
           <?php } ?>
      </div>
+     
      <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+         
      </button>
      <button class="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+         
      </button>
 </div>
